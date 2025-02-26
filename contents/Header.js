804d246c039d43c22afee62eddf20a4653871e85
saidjonakr <147584603+saidjonakr@users.noreply.github.com>
2025-02-26 15:51:29 +0500

    Create Header.js

---
+++ contents/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Cool Notes App</h1>
+      <p>Organize Your Thoughts</p>
+    </header>
+  );
+};
+
+export default Header;
